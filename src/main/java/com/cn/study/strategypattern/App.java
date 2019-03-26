d8a7f52956f7286e9aa2935a52676c31dcc85878
chenst <861293572@qq.com>
2019-03-26 22:02:54 +0800

    策略模式

---
+++ src/main/java/com/cn/study/strategypattern/App.java
@@ -0,0 +1,13 @@
+package com.cn.study.strategypattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
